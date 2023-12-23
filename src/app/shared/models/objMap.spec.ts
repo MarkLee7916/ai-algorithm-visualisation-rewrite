588e184ded2e2716de84f2d7fedda399619aed43
Mark Lee <marklee7916@gmail.com>
2023-12-23 13:22:19 +0000

    add obj maps

---
+++ src/app/shared/models/objMap.spec.ts
@@ -0,0 +1,23 @@
+import { ObjMap } from './objMap';
+
+import _ from 'lodash';
+
+describe('Object map', () => {
+  it('Works for arbitrary nested object', () => {
+    type TestObjType = { x: number; y: string[] };
+
+    const testKey1 = { x: 1, y: ['1'] };
+    const testKey2 = { x: 2, y: ['2'] };
+
+    const objMap = new ObjMap<TestObjType, number>([[testKey1, 1]]);
+
+    objMap.set(testKey2, 2);
+
+    expect(objMap.get(_.cloneDeep(testKey1))).toBe(1);
+    expect(objMap.get(testKey2)).toBe(2);
+
+    testKey2.x = 100;
+
+    expect(objMap.get(testKey2)).toBe(undefined);
+  });
+});
