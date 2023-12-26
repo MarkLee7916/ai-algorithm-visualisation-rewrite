420fc0f906f8636c31f33aa83ad8b8e7b449f9a8
Mark Lee <marklee7916@gmail.com>
2023-12-26 20:39:46 +0000

    Tap from end of stream

--- src/app/pathfinding/services/animation-frames.service.ts
+++ src/app/pathfinding/services/animation-frames.service.ts
@@ -32,8 +32,8 @@ export class AnimationFramesService {
                     // Calculate animation frames here
                     return [initBlankAnimationFrame(2, 2)];
                 }),
-                tap(frames => this.bridgeToAnotherStream.next(frames)),
                 take(1)
-            ))
+            )),
+            tap(frames => this.bridgeToAnotherStream.next(frames))
         );
 }
