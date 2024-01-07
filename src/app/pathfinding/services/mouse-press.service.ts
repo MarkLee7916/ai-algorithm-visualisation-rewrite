02d6836de5e4bed01a02027b64a87409d6abf5d5
Mark Lee <marklee7916@gmail.com>
2024-01-07 21:18:24 +0000

    Make dropping stop mouse being pressed

--- src/app/pathfinding/services/mouse-press.service.ts
+++ src/app/pathfinding/services/mouse-press.service.ts
@@ -1,14 +1,16 @@
 import { Inject, Injectable } from "@angular/core";
-import { merge, fromEvent, map } from "rxjs";
+import { merge, fromEvent, map, throttleTime } from "rxjs";
 import { StateService } from "./state.service";
 import { mousePress } from "../pathfinding.tokens";
 import { BridgeService } from "./bridge";
+import { DomUpdatesService } from "./dom-updates.service";
 
 @Injectable({
     providedIn: 'root',
 })
 export class MousePressService implements StateService<boolean>  {
     constructor(
+        private domUpdates: DomUpdatesService,
         @Inject(mousePress) bridgeToOtherStreams: BridgeService<boolean>,
     ) {
         bridgeToOtherStreams.link(this.getStream());
@@ -18,7 +20,14 @@ export class MousePressService implements StateService<boolean>  {
         return this.isMouseDown$;
     }
 
-    private isMouseDown$ = merge(fromEvent(document, 'mousedown'), fromEvent(document, 'mouseup')).pipe(
+    private handleDrop$ = this.domUpdates.drop$.pipe(
+        throttleTime(100),
+        map(() => false)
+    );
+
+    private handleMouseEvents$ = merge(fromEvent(document, 'mousedown'), fromEvent(document, 'mouseup')).pipe(
         map((event: Event) => (event as MouseEvent).buttons === 1)
     );
+
+    private isMouseDown$ = merge(this.handleDrop$, this.handleMouseEvents$);
 }
