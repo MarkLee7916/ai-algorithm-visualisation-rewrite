f255a0189be388e385dbd39b3c35ae7a4dbfb461
Mark Lee <marklee7916@gmail.com>
2024-01-13 19:00:00 +0000

    Enhance performance by avoiding lodash isEqual()

--- src/app/pathfinding/components/grid/grid.component.ts
+++ src/app/pathfinding/components/grid/grid.component.ts
@@ -1,16 +1,15 @@
 import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output, OnChanges, SimpleChanges } from "@angular/core";
 import { WeightGrid, weightAt } from "../../models/grid/weight-grid";
 import { BarrierGrid, hasBarrierAt } from "../../models/grid/barrier-grid";
-import { Pos } from "../../models/grid/pos";
+import { Pos, isSamePos } from "../../models/grid/pos";
 import { TypeOfDataDisplayedOnTileOption } from "../../models/dropdown/dropdown-enums";
 import { AnimationFrame } from "../../models/animation/animation-frame";
 import { height, width } from "../../models/grid/grid";
 import { pathLengthAt } from "../../models/grid/path-length-grid";
 import { frameAt } from "../../models/grid/animation-frame-grid";
-import { computeManhattanDist } from "../../algos/comparators";
 import { HeuristicDistFromGoalGrid, distAt } from "../../models/grid/heuristic-dist-from-goal-grid";
 import { TileEvent } from "../../models/grid/tile-event";
-import { isEqual, range } from "lodash";
+import { range } from "lodash";
 
 @Component({
     selector: 'app-grid',
@@ -33,12 +32,11 @@ export class GridComponent implements OnChanges {
     @Output() activate = new EventEmitter<Pos>();
 
     range = range;
-    isEqual = isEqual;
+    isSamePos = isSamePos;
     pathLengthAt = pathLengthAt;
     frameAt = frameAt;
     weightAt = weightAt;
     hasBarrierAt = hasBarrierAt;
-    computeManhattanDist = computeManhattanDist;
     distAt = distAt;
 
     gridHeight!: number;
